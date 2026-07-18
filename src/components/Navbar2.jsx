6a09453188d10a3c8db6473c0f2c4cf12daaab26
T-GAMER <dimasdevspro@gmail.com>
2026-07-18 14:53:59 -0300

    created cards by articles by blog

--- src/components/Navbar2.jsx
+++ src/components/Navbar2.jsx
@@ -9,7 +9,9 @@ export default function Navbar2() {
       {/* MOBILE (abaixo de 640px) */}
       <header className="block sm:hidden bg-blue-200 p-4">
         <div className="flex items-center justify-center gap-2">
-          <h2 className="text-center font-bold text-1xl">Blog</h2>
+          <h1 className="text-center font-bold text-1xl">
+            Blog de Astronomia, Biorritmos e Ciclos Naturais
+          </h1>
         </div>
         <nav className="mt-3 flex justify-center gap-4 text-lg">
           {user && (
@@ -28,7 +30,9 @@ export default function Navbar2() {
 
       {/* DESKTOP (acima de 640px) */}
       <header className="hidden sm:flex justify-between items-center bg-blue-200 py-4 px-8">
-        <h2 className="font-bold text-2xl">Blog</h2>
+        <h1 className="font-bold text-2xl">
+          Blog de Astronomia, Biorritmos e Ciclos Naturais
+        </h1>
         <div className="flex items-center gap-6">
           <nav className="flex gap-6 text-xl">
             {user && (
--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -22,39 +22,63 @@ export default function Blog() {
       />
       <Navbar2 />
       <div className="container mx-auto px-4 py-8 bg-blue-50 min-h-screen">
-        <p className="text-lg mb-8">Bem-vindo ao Blog.</p>
+        <p className="mb-6 text-lg leading-8">
+          Este blog reúne artigos sobre biorritmos, astronomia, história da
+          observação dos céus, ciclos naturais, fases da Lua, calendários
+          antigos e outros temas relacionados ao estudo do tempo e dos
+          movimentos celestes.
+        </p>
         {loading && <p className="text-lg">Carregando...</p>}
-        {posts.length === 0 && !loading && (
-          <div>
-            <p className="text-lg">Não foram encontrados posts</p>
-            <Link
-              to="/create-post"
-              className="text-lg bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 mt-2"
-            >
-              Criar primeiro post
-            </Link>
-          </div>
+        {!loading && posts.length === 0 && (
+          <>
+            <Navbar2 />
+            <div className="container mx-auto px-4 py-8 bg-blue-50 min-h-screen">
+              <h1 className="text-3xl font-bold mb-4">
+                Artigos não encontrados
+              </h1>
+              <p>Os artigos solicitados não existem ou foram removidos.</p>
+              <Link
+                to="/create-post"
+                className="text-lg bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 mt-2"
+              >
+                Criar primeiro post
+              </Link>
+            </div>
+          </>
         )}
 
         {posts.length > 0 && !loading && (
           <>
-            <div className="flex flex-row justify-between">
-              <h2 className="text-lg font-bold px-20">Título</h2>
-              <h2 className="text-lg font-bold px-20">Ações</h2>
-            </div>
-
             {posts.map((post) => (
               <div
                 key={post.id}
-                className="flex justify-between items-center border-blue-50 w-[100%] py-2 px-2 pr-20"
+                className="flex flex-col justify-between items-left border-blue-50 w-[100%] py-2 px-2 pr-20"
               >
-                <p className="text-lg">{post.title}</p>
+                <img className="w-80" src={post.imageUrl} alt={post.title} />
+                <h2 className="text-lg font-bold">{post.title}</h2>
+                <p className="text-sm">
+                  Publicado em:{" "}
+                  {post.createdAt.toDate().toLocaleDateString("pt-BR", {
+                    day: "2-digit",
+                    month: "2-digit",
+                    year: "numeric",
+                  })}
+                </p>
+                <p className="text-base">{post.body.blocks[1].data.text}...</p>
+                <p className="text-base flex flex-row">
+                  {post.tagsArray.map((tag) => (
+                    <p className="mr-4" key={tag}>
+                      <span className="text-sm font-bold">#</span>
+                      {tag}
+                    </p>
+                  ))}
+                </p>
                 <div className="flex flex-row justify-between gap-2">
                   <Link
                     to={`/post/${post.id}`}
-                    className="text-lg bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600"
+                    className="mb-4 text-lg bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600"
                   >
-                    Ver
+                    Ler artigo
                   </Link>
                 </div>
               </div>
